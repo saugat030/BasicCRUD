2146808319ed3d05904067fb2a44e2a07399c217
ZuluMumu <saugatdahal030@gmail.com>
2024-05-15 15:29:57 +0545

    edittttt

---
+++ MouseRazor/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MouseRazor.Pages.Categories
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- MouseRazor/Pages/Categories/create.cshtml.cs
+++ MouseRazor/Pages/Categories/create.cshtml.cs
@@ -4,10 +4,12 @@ using MouseRazor.Data;
 using MouseRazor.Model;
 
 namespace MouseRazor.Pages.Categories
-{
+{   //[BindProperties] This works as well but for all the properties in the page.
     public class createModel : PageModel
     {
+        
         private readonly MouseRazorDbContext _context;
+        [BindProperty] //This makes sure the OnPost recieves the Cat obj unlike in MVC it works slightly different. No need to put parameter in OnPost
         public Category Cat { get; set; } //since we dont need a list of category for creating but just the single Category instance Cat.
         public createModel(MouseRazorDbContext context)
         {
@@ -16,6 +18,12 @@ namespace MouseRazor.Pages.Categories
         public void OnGet()
         {
 
+        }
+        public IActionResult OnPost()
+        {
+            _context.Add(Cat); //direcly we can add this unlike in MVC compare it once to get clear details on how this works.
+            _context.SaveChanges();
+            return RedirectToPage("Index"); //redirect to action ko satta redirect to page.
         }
     }
 }
