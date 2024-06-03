3c254d6ff54cfc76c15064e5dad757992338f657
ZuluMumu <saugatdahal030@gmail.com>
2024-06-03 19:51:47 +0545

    repo class banyo

---
+++ Mouse.DataAccess/Repository/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mouse.DataAccess.Repository
+{
+    public interface IRepository<T> where T : class 
+    {
+        //T - Category or any other model we want to operate on.
+        IEnumerable<T> GetAll();
+        T Get(Expression<Func<T,bool>>filter);
+        void Add(T item);
+        void Remove(T item);
+       // void Update(T item); eslai chai generic repository ma halindaina coz update ko logic might be  a bit complicated sometimes. So update is created whenever needed.
+
+    }
+}
---
+++ Mouse.DataAccess/Repository/Repo.cs
@@ -0,0 +1,33 @@
+﻿using Mouse.DataAccess.Data;
+using Mouse.Models;
+
+namespace Mouse.DataAccess.Repository
+{
+    public class Repo<T> : IRepository<T> where T: class
+     {
+        private readonly MouseDbContext _db;
+        public Repo(MouseDbContext db)
+        {
+            _db=db;
+        }
+        public void Add(T item)
+        {
+            
+        }
+
+        public T Get(System.Linq.Expressions.Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
