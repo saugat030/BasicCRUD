906801ee8a465d8b44ac78040b3285e4a56c4005
Saugat Dahal <dahalsaugat@gmail.com>
2024-06-30 18:42:59 +0545

    Update ICategoryRepository.cs
    added more comments

--- Mouse.DataAccess/Repository/ICategoryRepository.cs
+++ Mouse.DataAccess/Repository/ICategoryRepository.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 
 namespace Mouse.DataAccess.Repository
 {
-    public  interface ICategoryRepository: IRepository<Categ>
-    {
+    public  interface ICategoryRepository: IRepository<Categ> //yo chai k vako ta vanda since the update logic for every repo might vary , we make a specific repo for each model.
+    { //Since this ICategegory repo ingerits the IRepository of type <Categ> , we can directly use this repo to inherit from in the CategoryRepository.
         void Update(Categ obj);
         
     }
