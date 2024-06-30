1d820773b0123881cf9b4279b2627ec46e88da68
Saugat Dahal <dahalsaugat@gmail.com>
2024-06-30 18:40:12 +0545

    Update Repo.cs
    commentd haleko

--- Mouse.DataAccess/Repository/Repo.cs
+++ Mouse.DataAccess/Repository/Repo.cs
@@ -1,18 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore;
+//Ya chai aba we work with the datacontext ko kura haru.
+using Microsoft.EntityFrameworkCore;
 using Mouse.DataAccess.Data;
 using Mouse.Models;
 using System.Linq.Expressions;
 namespace Mouse.DataAccess.Repository
 {
-    public class Repo<T> : IRepository<T> where T: class
+    public class Repo<T> : IRepository<T> where T: class //This again gets inherited later by another repo of another model we want to work on.
      {
-        private readonly MouseDbContext _db;
-        internal DbSet<T> dbset;
-        public Repo(MouseDbContext db)
+        private readonly MouseDbContext _db; 
+        internal DbSet<T> dbset;      //Just making a Dbset object since you cant do a _db.Dbset (We dont know the tye of the class ,It can me multiple tables sice 1 db set represents 1 table.)
+        public Repo(MouseDbContext db) // This db comes from the constructor dependency injection in the controller.
         {
             _db=db;
             this.dbset= _db.Set<T>();
-            //_db.Categs == dbSet; same shit
+            //_db.Categs == dbSet; same shit yo pani alik imp kura xa. 
 
         }
         public void Add(T item)
